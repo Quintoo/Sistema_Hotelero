c36779890d67c10a0b762bbb05ace6a7d600323a
Ricardo ZP <pzrico@hotmail.com>
2019-11-12 10:59:09 -0600

    base de datos
    estructua de la base de datos y archivos

--- vista/reservacion.php
+++ vista/reservacion.php
@@ -12,7 +12,7 @@
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-  <link href="css/pagina2.min.css" rel="stylesheet">
+  <link href="../css/pagina2.min.css" rel="stylesheet">
 
 </head>
 
@@ -28,7 +28,7 @@
       <div class="collapse navbar-collapse" id="navbarResponsive">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="index.html">Inicio</a>
+            <a class="nav-link" href="panel.php">Inicio</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="about.html">Introduccion</a>
@@ -45,7 +45,7 @@
   </nav>
 
   <!-- Page Header -->
-  <header class="masthead" style="background-image: url('img/7.jpg')">
+  <header class="masthead" style="background-image: url('../img/7.jpg')">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
